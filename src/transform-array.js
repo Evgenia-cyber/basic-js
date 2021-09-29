91d81038f377e77f8be0372e39d71e95998fc420
Evgenia Kurineva <kurineva85@mail.ru>
2021-09-29 12:53:00 +0300

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,17 +3,50 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+  
+  if (arr.length === 0) {
+    return [];
+  }
+
+  // if (!Array.isArray(arr)) {
+  //   throw new Error("'arr' parameter must be an instance of the Array!");
+  // }
+
+  // return arr.reduce((res, item, index) => {
+  //   if (item === '--discard-prev') res.pop();
+  //   if (item === '--discard-next')
+  //     if (item === '--double-prev') res.push(arr[index - 1]);
+  //   if (item === '--double-next') res.push(arr[index + 1]);
+  //   else {
+  //     res.push(item);
+  //   }
+  //   return res;
+  // }, []);
+
+  const res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-prev') res.pop();
+    else if (arr[i] === '--discard-next') i = i + 2;
+    else if (arr[i] === '--double-prev') res.push(arr[i - 1]);
+    else if (arr[i] === '--double-next') res.push(arr[i + 1]);
+    else if (arr[i] !== undefined) res.push(arr[i]);
+  }
+
+  return res.filter((item) => item !== undefined);
 }
